7beeb53e550edb6dd37648188a4245a24a5c98ef
Adam Gundry <adam.gundry@strath.ac.uk>
2013-08-15 12:05:40 +0100

    Test OverloadedRecordFields with TemplateHaskell

---
+++ tests/overloadedrecflds/should_run/overloadedrecfldsrun04.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE OverloadedRecordFields, TemplateHaskell #-}
+
+import GHC.Records
+import Language.Haskell.TH
+import Language.Haskell.TH.Syntax
+
+-- Splice in a datatype with field...
+$(return [DataD [] (mkName "R") [] [RecC (mkName "MkR") [(mkName "foo", NotStrict, ConT ''Int)]] []])
+
+-- ... and check that we can inspect it
+main = do  putStrLn $(do { info <- reify ''R
+                         ; lift (pprint info) })
+           putStrLn $(do { insts <- reifyInstances ''Has [ConT ''R, LitT (StrTyLit "foo"), ConT ''Int]
+                         ; lift (pprint insts) })
+           print (foo (MkR { foo = 42 }))
